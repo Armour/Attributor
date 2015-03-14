37ebc3a10ddb2263926d20190f59ed24c44a6436
Armour <497052684@qq.com>
2015-03-14 12:23:23 +0800

    add outline button

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -12,11 +12,22 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *headline;
 @property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle ];
+    [title setAttributes:@{NSStrokeWidthAttributeName: @3,
+                           NSStrokeColorAttributeName: self.outlineButton.tintColor}
+                    range:NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
 
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfBotton:(UIButton *)sender {
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName
@@ -32,11 +43,6 @@
 
 - (IBAction)unoutlineBodySelection {
     [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 @end
