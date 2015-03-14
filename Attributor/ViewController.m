5a9e391731c975ffce3be790910ff69949134fc0
Armour <497052684@qq.com>
2015-03-14 09:09:26 +0800

    initial commit / with foundational function

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -10,18 +10,33 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UITextView *body;
+
 @end
 
+
 @implementation ViewController
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfBotton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection {
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName: @-3,
+                                           NSStrokeColorAttributeName: [UIColor blackColor]}
+                                   range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection {
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName range:self.body.selectedRange];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
